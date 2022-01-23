038c9c1f9f30260c8829de94971941548f9f0740
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-01-23 14:02:38 +0200

    Create create.blade.php

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <form action="{{ route('tasks.store') }}" method="POST">
+        @csrf
+        <div class="card">
+            <div class="card-header">Create task</div>
+
+            <div class="card-body">
+                <div class="form-group">
+                    <label class="required" for="title">Title</label>
+                    <input class="form-control" type="text" name="title" id="title" required>
+                    <span class="help-block"> </span>
+                </div>
+
+                <div class="form-group">
+                    <label class="required" for="description">Description</label>
+                    <textarea class="form-control" rows="10" name="description" id="description"></textarea>
+                    <span class="help-block"> </span>
+                </div>
+
+                <div class="form-group">
+                    <label for="deadline">Deadline</label>
+                    <input class="form-control" type="date" name="deadline" id="deadline">
+                    <span class="help-block"> </span>
+                </div>
+
+                <div class="form-group">
+                    <label for="user_id">Assigned user</label>
+                    <select class="form-control" name="user_id" id="user_id" required>
+                        @foreach ($users as $id => $entry)
+                            <option value="{{ $id }}" {{ $id == 'id' ? 'selected' : '' }}>{{ $entry }}
+                            </option>
+                        @endforeach
+                    </select>
+
+                    <span class="help-block"> </span>
+                </div>
+
+                <div class="form-group">
+                    <label for="client_id">Assigned client</label>
+                    <select class="form-control" name="client_id" id="client_id" required>
+                        @foreach ($clients as $id => $entry)
+                            <option value="{{ $id }}" {{ $id == 'id' ? 'selected' : '' }}>{{ $entry }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <span class="help-block"> </span>
+                </div>
+
+                <div class="form-group">
+                    <label for="client_id">Assigned project</label>
+                    <select class="form-control" name="project_id" id="project_id" required>
+                        @foreach ($projects as $id => $entry)
+                            <option value="{{ $id }}" {{ $id == 'id' ? 'selected' : '' }}>{{ $entry }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <span class="help-block"> </span>
+                </div>
+
+                <div class="form-group">
+                    <label for="status">Status</label>
+                    <select class="form-control" name="status" id="status" required>
+                        @foreach (App\Models\Task::status as $item)
+                            <option value="{{ $item }}" {{ $item == 'status' ? 'selected' : '' }}>{{ $item }}
+                            </option>
+                        @endforeach
+                    </select>
+
+                    <span class="help-block"> </span>
+                </div>
+
+                <button class="btn btn-primary" type="submit">
+                    Save
+                </button>
+            </div>
+        </div>
+
+    </form>
+
+@endsection

6176fb80963d8e983334fb2a6a71440adca863b6
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-01-23 10:58:56 +0200

    Create 2022_01_23_085059_create_tasks_table.php

---
+++ database/migrations/2022_01_23_085059_create_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->longText('description');
+            $table->integer('user_id')->unsigned();
+            $table->integer('project_id')->unsigned();
+            $table->date('deadline');
+            $table->string('status')->default('open');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}

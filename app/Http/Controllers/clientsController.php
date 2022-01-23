e9097af7264ce92ee03b9fcdbd6faf65b823cbf9
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-01-23 16:52:07 +0200

    Update clientsController.php

--- app/Http/Controllers/clientsController.php
+++ app/Http/Controllers/clientsController.php
@@ -15,7 +15,7 @@ class clientsController extends Controller
      */
     public function index()
     {
-        $clients = Client::all();
+        $clients = Client::paginate(20);
         return view('clients.index')->with('clients',$clients);
     }
 

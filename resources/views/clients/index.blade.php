a3f84c7a191bd31b791767b0ccf8917c5fdc591a
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-01-23 16:52:44 +0200

    Update index.blade.php

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -58,7 +58,7 @@
                 </tbody>
             </table>
 
-            {{-- {{ $clients->withQueryString()->links() }} --}}
+            {{ $clients->withQueryString()->links() }}
         </div>
     </div>
 

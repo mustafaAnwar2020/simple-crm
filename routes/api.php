7019b5d6a965ae49b3e760f7d3a7a40fff1f600b
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-01-29 16:17:22 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::post('/login','App\Http\Controllers\API\LoginController@login');
 Route::post('/register','App\Http\Controllers\API\RegiserController@register');
 
@@ -25,16 +22,17 @@ Route::middleware('auth:api')->group(function () {
     Route::put('/profile/update/','App\Http\Controllers\API\ProfileController@update');
     Route::put('/profile/updatePassword/','App\Http\Controllers\API\ProfileController@updatePassword');
     Route::get('/clients','App\Http\Controllers\API\ClientsController@index');
-    Route::get('/clients/trash','App\Http\Controllers\API\ClientsController@trashedClients');
+    // Route::get('/clients/trash','App\Http\Controllers\API\ClientsController@trashedClients');
     Route::post('/clients/create','App\Http\Controllers\API\ClientsController@store');
     Route::put('/clients/update/{id}','App\Http\Controllers\API\ClientsController@update');
     Route::delete('/clients/{id}','App\Http\Controllers\API\ClientsController@destroy');
     // Route::delete('/clients/delete/{id}','App\Http\Controllers\API\ClientsController@delete');
     // Route::get('/clients/restore/{id}','App\Http\Controllers\API\ClientsController@restore');
     Route::get('/projects','App\Http\Controllers\API\ProjectsController@index');
-    Route::get('/projects/trash','App\Http\Controllers\API\ProjectsController@trashedProjects');
+    // Route::get('/projects/trash','App\Http\Controllers\API\ProjectsController@trashedProjects');
     Route::get('/projects/show/{id}','App\Http\Controllers\API\ProjectsController@show');
     Route::post('/projects/create','App\Http\Controllers\API\ProjectsController@store');
     Route::put('/projects/update/{id}','App\Http\Controllers\API\ProjectsController@update');
     Route::delete('/projects/{id}','App\Http\Controllers\API\ProjectsController@destroy');
+    Route::apiResource('/Tasks','App\Http\Controllers\API\TasksController');
 });

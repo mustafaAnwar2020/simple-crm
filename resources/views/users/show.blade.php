1ab5d655842e4382156c69239a4877dc1aef87ea
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-05 00:01:43 +0200

    Create show.blade.php

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">User info</div>
+
+                <div class="card-body">
+                    <p class="mb-0">{{ $user->name }}</p>
+                    <p class="mb-0">{{ $user->email }}</p>
+                    <p class="mb-0">{{ $user->profile->phone }}</p>
+                    <p class="mb-0">{{ $user->profile->address }}</p>
+                    <p class="mb-0">{{ $user->profile->contact }}</p>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Assigned projects</div>
+                @foreach ($projects as $item)
+                    <div class="card-body">
+
+                        <p class="mb-0">{{ $item->title }}</p>
+                        <p class="mb-0">{{ $item->description }}</p>
+
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card card-accent-primary">
+                <div class="card-header">Assigned tasks</div>
+
+                @foreach ($tasks as $item)
+                    <div class="card-body">
+
+                        <p class="mb-0">{{ $item->title }}</p>
+                        <p class="mb-0">{{ $item->description }}</p>
+
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+    </div>
+@endsection

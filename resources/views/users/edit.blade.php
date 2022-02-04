e1d1aca639c97730819eeece926570b105448de8
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-04 23:33:48 +0200

    Create edit.blade.php

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="card">
+    <div class="card-header">Edit User</div>
+
+    <div class="card-body">
+        <form action="{{route('users.update',$user)}}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label class="required" for="first_name">name</label>
+                <input class="form-control" type="text" name="name" value="{{$user->name}}" id="name" required>
+                <span class="help-block"> </span>
+            </div>
+
+            <div class="form-group">
+                <label class="required" for="first_name">email</label>
+                <input class="form-control" type="email" name="email" value="{{$user->email}}" id="name" required>
+                <span class="help-block"> </span>
+            </div>
+
+            <div class="form-group">
+                <label class="required" for="address">Address</label>
+                <input class="form-control" type="text" name="address" value="{{$user->profile->address}}" id="address"  required>
+                <span class="help-block"> </span>
+            </div>
+
+            <div class="form-group">
+                <label class="required" for="phone_number">Phone number</label>
+                <input class="form-control" type="text" name="phone" value="{{$user->profile->phone}}" id="phone_number"  required>
+                <span class="help-block"> </span>
+            </div>
+
+            <div class="form-group">
+                <label class="required" for="phone_number">Contact</label>
+                <input class="form-control" type="text" name="contact" value="{{$user->profile->contact}}" id="phone_number"  required>
+                <span class="help-block"> </span>
+            </div>
+
+            <button class="btn btn-primary" type="submit">
+                Save
+            </button>
+
+
+
+        </form>
+    </div>
+</div>
+
+@endsection

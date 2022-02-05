b2f085b593e47a6265da3b07b1dc9c5247837bd3
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-06 01:51:05 +0200

    add permissions

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -5,36 +5,45 @@
             Dashboard
         </a>
     </li>
-    {{-- @can('manage users')
+    @can('user-list')
         <li class="c-sidebar-nav-item">
-            <a class="c-sidebar-nav-link {{ request()->is('users/*') ? 'c-active' : '' }}"
-                href="{{ route('users.index') }}">
+            <a class="c-sidebar-nav-link" href="{{ route('users.index') }}">
                 <i class="c-sidebar-nav-icon fas fa-fw fa-user-alt"></i>
                 Users
             </a>
         </li>
-    @endcan --}}
+    @endcan
     <li class="c-sidebar-nav-item">
-        <a class="c-sidebar-nav-link"
-            href="{{ route('clients.index') }}">
+        <a class="c-sidebar-nav-link" href="{{ route('clients.index') }}">
             <i class="c-sidebar-nav-icon fas fa-fw fa-address-card"></i>
             Clients
         </a>
     </li>
+    @can('project-list')
     <li class="c-sidebar-nav-item">
-        <a class="c-sidebar-nav-link"
-            href="{{route('projects.index')}}">
+        <a class="c-sidebar-nav-link" href="{{ route('projects.index') }}">
             <i class="c-sidebar-nav-icon fas fa-fw fa-copy"></i>
             Projects
         </a>
     </li>
+    @endcan
+    @can('task-list')
     <li class="c-sidebar-nav-item">
-        <a class="c-sidebar-nav-link"
-            href="{{route('tasks.index')}}">
+        <a class="c-sidebar-nav-link" href="{{ route('tasks.index') }}">
             <i class="c-sidebar-nav-icon fas fa-fw fa-tasks"></i>
             Tasks
         </a>
     </li>
+    @endcan
+
+    @can('role-list')
+        <li class="c-sidebar-nav-item">
+            <a class="c-sidebar-nav-link" href="{{ route('roles.index') }}">
+                <i class="c-sidebar-nav-icon fas fa-fw fa-user-cog"></i>
+                Roles
+            </a>
+        </li>
+    @endcan
     <li class="c-sidebar-nav-divider"></li>
     <li class="c-sidebar-nav-item mt-auto"></li>
     <li class="c-sidebar-nav-item"><a href="#" class="c-sidebar-nav-link"

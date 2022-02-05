9036fce8fe1e4a0c685e4416a1ceec6b5897f7c6
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-06 01:53:12 +0200

    Update RolesController.php

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -10,13 +10,13 @@ use Illuminate\Support\Facades\DB;
 class RolesController extends Controller
 {
 
-    // function __construct()
-    // {
-    //     $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index', 'store']]);
-    //     $this->middleware('permission:role-create', ['only' => ['create', 'store']]);
-    //     $this->middleware('permission:role-edit', ['only' => ['edit', 'update']]);
-    //     $this->middleware('permission:role-delete', ['only' => ['destroy']]);
-    // }
+     function __construct()
+     {
+         $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index', 'store']]);
+         $this->middleware('permission:role-create', ['only' => ['create', 'store']]);
+         $this->middleware('permission:role-edit', ['only' => ['edit', 'update']]);
+         $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+     }
     public function index()
     {
         $roles = Role::paginate(5);

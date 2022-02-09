e836d3fdfae15e3badf1b4d97f3e98134b07110d
Mustafa Anwar <35630821+mustafaAnwar2020@users.noreply.github.com>
2022-02-09 04:50:51 +0200

    Update user-dropdown.blade.php

--- resources/views/partials/user-dropdown.blade.php
+++ resources/views/partials/user-dropdown.blade.php
@@ -1,12 +1,12 @@
 <ul class="c-header-nav ml-auto mr-4">
-    {{-- <li class="c-header-nav-item d-md-down-none mx-2">
+    <li class="c-header-nav-item d-md-down-none mx-2">
         <a class="c-header-nav-link" href="{{ route('notifications.index') }}">
             @if (auth()->user()->unreadNotifications->count())
                 <span class="badge rounded-pill bg-success">{{ auth()->user()->unreadNotifications->count() }}</span>
             @endif
             <i class="far fa-bell"></i>
         </a>
-    </li> --}}
+    </li> 
     <li class="c-header-nav-item dropdown">
         <a class="c-header-nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
             aria-expanded="false">
